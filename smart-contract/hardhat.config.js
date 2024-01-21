487d7f3902b636ee14143ab6691510b177b7c32c
VirajAdiga <virajadigamaclub@gmail.com>
2024-01-21 13:19:59 +0530

    Add deploy script and config to deploy Lottery contract

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -3,4 +3,13 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      loggingEnabled: true,
+      accounts: {
+        mnemonic: "test1 test2 test3 test4 test5 test6 test7 test8 test9 test10 test11 test12"
+      },
+    },
+  },
 };
